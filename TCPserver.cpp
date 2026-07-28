c25de8f6bf56985c92ff8dd0d0821f388a98a5a9
Sanad Mac <sanadatia.07@gmail.com>
2026-07-27 22:01:08 -0700

    structs and busts

--- TCPserver.cpp
+++ TCPserver.cpp
@@ -7,32 +7,30 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <thread>
+#include <vector>
 
 constexpr int PORT = 8080;
 constexpr int BUFFER_SIZE = 1024;
 
-// struct threadSock
-// {
-//     std::thread currThread;
-//     int currSocket;
+//struct to hold thread-socket pairs for iteration
+struct threadSock
+{
+    std::thread thread;
+    int socket;
+    
+    //parameterized constructor 
+    threadSock(std::thread&& t, int s) : thread(std::move(t)), socket(s) {}
+};
 
-//     // Constructor with parameters
-//     threadSock(std::thread t, int s)
-//     {
-//         currThread = t;
-//         currSocket = s;
-//     }
-// };
-
-
-void msgThread(int new_socket, char* buffer)
+//function to handle messaging between clients - passed to threads
+void msgThread(int new_socket, char* buffer, std::vector<threadSock> &threads)
 {
     while (true)
     {
         ssize_t valread = read(new_socket, buffer, BUFFER_SIZE - 1);
         //null terminate after read message
         buffer[valread] = '\0';
-        //break upon client termination
+        //break upon client disconnect
         if (valread <= 0) { break; }
 
         //echo message 
@@ -49,11 +47,14 @@ int main() {
     int opt = 1;
     socklen_t addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
+    std::vector<threadSock> threads;
+
     //creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
+
     // //forcefully attaching socket to the port 8080
     // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
     //     perror("setsockopt");
@@ -64,37 +65,41 @@ int main() {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
+
     //bind the socket to the network address and port
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+
     //start listening for incoming connections
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
     std::cout << "Server listening on port " << PORT << std::endl;
+
     //accept incoming connection
-    while (true) // this will useful once threads are introduced
+    while (true)
     {
         if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) 
         {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-        std::thread tester(msgThread, new_socket, buffer);
-
-        tester.detach();
-        // std::cout << "Client connectd." << std::endl; TODO
+        //create thread to begin messaging
+        std::thread messenger(msgThread, new_socket, buffer, threads);
+        //add thread to client list
+        threads.emplace_back(std::move(messenger), new_socket);
+        //detach thread, continue looping for clients
+        threads.back().thread.detach();
+        //std::cout << "Clients connectd: " << threads.size() << std::endl;
     }
-    //read and echo the received message
-    //msgThread(new_socket, buffer);
-    //close the socket
-    //close(new_socket);
+
     close(server_fd);
     return 0;
 }   
